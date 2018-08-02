d5f446f336b12cfa233d8f28eede4e1d6b3554f9
kim han woong <khw7096@gmail.com>
2018-08-02 14:10:49 +0900

    전부 넘버로 변경함.

--- upload.go
+++ upload.go
@@ -9,9 +9,9 @@ func main() {
 	regionPtr := flag.String("region", "ap-northeast-2", "AWS region name.") // 현재 Lazypic은 서울리전을 사용중이다.
 	bucketPtr := flag.String("bucket", "lazypic", "S3 bucket name.")
 	projectPtr := flag.String("project", "", "project name")
-	episodePtr := flag.Int("episode", 0, "project name")
-	scenePtr := flag.String("scene", "", "project name")
-	cutPtr := flag.String("cut", "", "project name")
+	episodePtr := flag.Int("episode", 0, "episode number")
+	scenePtr := flag.Int("scene", 0, "scene number")
+	cutPtr := flag.Int("cut", 0, "cut number")
 	filePtr := flag.String("file", "", "project name")
 	flag.Parse()
 	fmt.Println(*regionPtr)
