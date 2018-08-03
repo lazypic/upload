4f3286511fc0280ee4a505bdf80447d3638cdc89
kim han woong <khw7096@gmail.com>
2018-08-03 18:35:50 +0900

    빌드 추가함.

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
-GOOS=linux GOARCH=amd64 go build -o ./bin/upload upload.go
-GOOS=windows GOARCH=amd64 go build -o ./bin/upload.exe upload.go
-GOOS=darwin GOARCH=amd64 go build -o ./bin/upload upload.go
+# OS별로 빌드함.
+GOOS=linux GOARCH=amd64 go build -o ./bin/linux/upload upload.go
+GOOS=windows GOARCH=amd64 go build -o ./bin/windows/upload.exe upload.go
+GOOS=darwin GOARCH=amd64 go build -o ./bin/darwin/upload upload.go
+
+# Github Release에 업로드 하기위해 압축
+cd ./bin/linux/ && tar -zcvf ../upload_linux.tgz . && cd -
+cd ./bin/windows/ && tar -zcvf ../upload_windows.tgz . && cd -
+cd ./bin/darwin/ && tar -zcvf ../upload_darwin.tgz . && cd -
